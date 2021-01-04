9095d030d618eecdc7dd84a8cab9cb398d3ea2d1
diegorod <me@diegorod.com>
2021-01-04 15:17:48 -0500

    Add project files.

---
+++ Models.cs
@@ -0,0 +1,338 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VistarApiClient
+{
+	public class VistarReponseException : Exception
+	{
+		public VistarErrorResponse Error { get; set; }
+		public VistarReponseException(VistarErrorResponse error)
+		{
+			Error = error;
+		}
+	}
+
+	public class VistarErrorResponse
+	{
+		[JsonProperty("message")]
+		public string Message { get; set; }
+
+		[JsonProperty("code")]
+		public long Code { get; set; }
+
+		[JsonProperty("details")]
+		public ErrorDetail[] Details { get; set; }
+	}
+
+	public class ErrorDetail
+	{
+		[JsonProperty("@type")]
+		public string Type { get; set; }
+
+		[JsonProperty("field_violations")]
+		public FieldViolation[] FieldViolations { get; set; }
+	}
+
+	public class FieldViolation
+	{
+		[JsonProperty("field")]
+		public string Field { get; set; }
+
+		[JsonProperty("description")]
+		public string Description { get; set; }
+	}
+
+	public class VistarVenuePayload
+	{
+		[JsonProperty("creative_category_restrictions")]
+		public CreativeCategoryRestrictions CreativeCategoryRestrictions { get; set; }
+
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("venue_type")]
+		public string VenueType { get; set; }
+
+		[JsonProperty("excluded_buy_types")]
+		public List<string> ExcludedBuyTypes { get; set; }
+
+		[JsonProperty("network_id")]
+		public string NetworkId { get; set; }
+
+		[JsonProperty("longitude")]
+		public double Longitude { get; set; }
+
+		[JsonProperty("advertiser_restrictions")]
+		public AdvertiserRestrictions AdvertiserRestrictions { get; set; }
+
+		[JsonProperty("operating_minutes")]
+		public List<OperatingMinute> OperatingMinutes { get; set; }
+
+		[JsonProperty("activation_date")]
+		public DateTime? ActivationDate { get; set; }
+
+		[JsonProperty("cpm_floor_cents")]
+		public long CpmFloorCents { get; set; }
+
+		[JsonProperty("partner_venue_id")]
+		public string PartnerVenueId { get; set; }
+
+		[JsonProperty("address")]
+		public string Address { get; set; }
+
+		[JsonProperty("latitude")]
+		public double Latitude { get; set; }
+
+		[JsonProperty("impressions")]
+		public Impressions Impressions { get; set; }
+
+		[JsonProperty("gvt")]
+		public Gvt Gvt { get; set; }
+
+		//	[JsonProperty("video_supported")]
+		//	public bool VideoSupported { get; set; }
+		//
+		//	[JsonProperty("static_supported")]
+		//	public bool StaticSupported { get; set; }
+		//
+		//	[JsonProperty("cortex_supported")]
+		//	public bool CortexSupported { get; set; }
+		//
+		//	[JsonProperty("registration_id")]
+		//	public string RegistrationId { get; set; }
+
+		//	[JsonProperty("height_px")]
+		//	public int HeightPx { get; set; }
+		//
+		//	[JsonProperty("width_px")]
+		//	public int WidthPx { get; set; }
+
+		//[JsonProperty("static_duration_seconds")]
+		//public int StaticDurationSeconds { get; set; }
+	}
+
+	public class VistarVenueUpdatePayload : VistarVenuePayload
+	{
+		[JsonProperty("location_id")]
+		public string LocationId { get; set; }
+		[JsonProperty("id")]
+		public string Id { get; set; }
+	}
+
+	public class VistarUser
+	{
+		[JsonProperty("username")]
+		public string Username { get; set; }
+
+		[JsonProperty("is_buyer")]
+		public bool IsBuyer { get; set; }
+
+		[JsonProperty("abilities")]
+		public Dictionary<string, string[]> Abilities { get; set; }
+
+		[JsonProperty("public_id")]
+		public string PublicId { get; set; }
+
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("allow_letterbox")]
+		public bool AllowLetterbox { get; set; }
+
+		[JsonProperty("is_multi_authed")]
+		public bool IsMultiAuthed { get; set; }
+
+		[JsonProperty("locale")]
+		public string Locale { get; set; }
+
+		[JsonProperty("country")]
+		public string Country { get; set; }
+
+		[JsonProperty("client_fee_bp")]
+		public long ClientFeeBp { get; set; }
+
+		[JsonProperty("account_id")]
+		public string AccountId { get; set; }
+
+		[JsonProperty("id")]
+		public string Id { get; set; }
+
+		[JsonProperty("currency")]
+		public string Currency { get; set; }
+
+		[JsonProperty("role_name")]
+		public string RoleName { get; set; }
+
+		[JsonProperty("is_admin")]
+		public bool IsAdmin { get; set; }
+
+		[JsonProperty("is_multi_account")]
+		public bool IsMultiAccount { get; set; }
+
+		[JsonProperty("is_seller")]
+		public bool IsSeller { get; set; }
+
+		[JsonProperty("homepage")]
+		public string Homepage { get; set; }
+
+		[JsonProperty("email")]
+		public string Email { get; set; }
+
+		[JsonProperty("screen_count_enabled")]
+		public bool ScreenCountEnabled { get; set; }
+
+		[JsonProperty("account_email")]
+		public string AccountEmail { get; set; }
+	}
+
+	public class VistarVenueResultList
+	{
+		[JsonProperty("venues")]
+		public List<VistarVenue> Venues { get; set; }
+	}
+
+	public class VistarVenue
+	{
+		[JsonProperty("venue_type_id")]
+		public long VenueTypeId { get; set; }
+
+		[JsonProperty("creative_category_restrictions")]
+		public CreativeCategoryRestrictions CreativeCategoryRestrictions { get; set; }
+
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("venue_type")]
+		public string VenueType { get; set; }
+
+		[JsonProperty("excluded_buy_types")]
+		public List<string> ExcludedBuyTypes { get; set; }
+
+		[JsonProperty("network_id")]
+		public string NetworkId { get; set; }
+
+		[JsonProperty("longitude")]
+		public double Longitude { get; set; }
+
+		[JsonProperty("advertiser_restrictions")]
+		public AdvertiserRestrictions AdvertiserRestrictions { get; set; }
+
+		[JsonProperty("operating_minutes")]
+		public List<OperatingMinute> OperatingMinutes { get; set; }
+
+		[JsonProperty("activation_date")]
+		public DateTime? ActivationDate { get; set; }
+
+		[JsonProperty("gvt")]
+		public Gvt Gvt { get; set; }
+
+		[JsonProperty("partner_venue_id")]
+		public string PartnerVenueId { get; set; }
+
+		[JsonProperty("address")]
+		public string Address { get; set; }
+
+		[JsonProperty("latitude")]
+		public double Latitude { get; set; }
+
+		[JsonProperty("impressions")]
+		public Impressions Impressions { get; set; }
+
+		[JsonProperty("location_id")]
+		public string LocationId { get; set; }
+
+		[JsonProperty("id")]
+		public string Id { get; set; }
+
+		[JsonProperty("cpm_floor_cents")]
+		public long CpmFloorCents { get; set; }
+
+		[JsonProperty("video_supported")]
+		public bool VideoSupported { get; set; }
+
+		[JsonProperty("static_supported")]
+		public bool StaticSupported { get; set; }
+
+		[JsonProperty("cortex_supported")]
+		public bool CortexSupported { get; set; }
+
+		[JsonProperty("registration_id")]
+		public string RegistrationId { get; set; }
+
+		[JsonProperty("height_px")]
+		public int HeightPx { get; set; }
+
+		[JsonProperty("width_px")]
+		public int WidthPx { get; set; }
+
+		[JsonProperty("static_duration_seconds")]
+		public int StaticDurationSeconds { get; set; }
+	}
+
+	public class AdvertiserRestrictions
+	{
+		[JsonProperty("exclude")]
+		public bool Exclude { get; set; }
+
+		[JsonProperty("advertiser_ids")]
+		public List<string> AdvertiserIds { get; set; }
+	}
+
+	public class CreativeCategoryRestrictions
+	{
+		[JsonProperty("exclude")]
+		public bool Exclude { get; set; }
+
+		[JsonProperty("categories")]
+		public List<string> Categories { get; set; }
+	}
+
+	public class Gvt
+	{
+		[JsonProperty("seconds")]
+		public long Seconds { get; set; }
+
+		[JsonProperty("impressions")]
+		public long Impressions { get; set; }
+
+		[JsonProperty("dwell_time_seconds")]
+		public long DwellTimeSeconds { get; set; }
+	}
+
+	public class Impressions
+	{
+		[JsonProperty("per_spot")]
+		public double PerSpot { get; set; }
+
+		[JsonProperty("per_second")]
+		public double PerSecond { get; set; }
+	}
+
+	public class OperatingMinute
+	{
+		[JsonProperty("local_end_minute_of_week")]
+		public int LocalEndMinuteOfWeek { get; set; }
+
+		[JsonProperty("local_start_minute_of_week")]
+		public int LocalStartMinuteOfWeek { get; set; }
+
+		public OperatingMinute()
+		{
+
+		}
+		/// <summary>
+		/// Monday = 0 -> Sunday = 6
+		/// </summary>
+		/// <param name="openTime"></param>
+		/// <param name="closeTime"></param>
+		/// <param name="day"></param>
+		public OperatingMinute(TimeSpan openTime, TimeSpan closeTime, int day)
+		{
+			//Day of week (0-6) * minutes in an hour (60) * hours in a day (24) + hour of day (0-23) * minutes in an hour (60) + minute of hour (0-59).
+			this.LocalStartMinuteOfWeek = day * 60 * 24 + openTime.Hours * 60 + openTime.Minutes;
+			this.LocalEndMinuteOfWeek = day * 60 * 24 + closeTime.Hours * 60 + closeTime.Minutes;
+		}
+	}
+}
---
+++ VistarClient.cs
@@ -0,0 +1,207 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace VistarApiClient
+{
+	public class VistarClient
+	{
+		class LoginCredentials
+		{
+			public string email { get; set; }
+			public string password { get; set; }
+		}
+		public RestClient Client { get; private set; }
+		public VistarUser User { get; private set; }
+		private LoginCredentials LoginInfo;
+		public bool Staging { get; set; }
+		private string BaseApiUrl => Staging ? "https://staging.vistarmedia.com" : "https://trafficking.vistarmedia.com";
+		private string ApiUriBuilder(string uri) => BaseApiUrl + uri;
+		private string AuthCookieId => Staging ? "tr-staging" : "tr-production";
+
+		/// <summary>
+		/// Creates a new instance of the client. Use Connect() method to attempt authentication. 
+		/// </summary>
+		/// <param name="username"></param>
+		/// <param name="password"></param>
+		/// <param name="staging"></param>
+		/// <param name="restClientTimeOut"></param>
+		public VistarClient(string username, string password, bool staging = true, int restClientTimeOut = -1)
+		{
+			Staging = staging;
+			Client = new RestClient(BaseApiUrl);
+			Client.Timeout = restClientTimeOut;
+			LoginInfo = new LoginCredentials()
+			{
+				email = username,
+				password = password
+			};
+		}
+
+		private static class VistarApiUri
+		{
+			public static string Authenticate = "/session/";
+			public static string VenuesBase = "/seller/v1/venues/";
+			public static string SingleVenue(string id) => $"{VenuesBase}{id}";
+			public static string DeleteVenue(string id) => $"{VenuesBase}{id}";
+		}
+
+		public bool Connect()
+		{
+			var request = new RestRequest(VistarApiUri.Authenticate, Method.POST);
+			request.AddHeader("Content-Type", "text/plain");
+			request.AddParameter("text/plain", JsonConvert.SerializeObject(LoginInfo), ParameterType.RequestBody);
+			IRestResponse response = Client.Execute(request);
+
+			if (response.StatusCode == HttpStatusCode.OK)
+			{
+				User = JsonConvert.DeserializeObject<VistarUser>(response.Content);
+				var authCookie = response.Cookies.FirstOrDefault(x => x.Name == AuthCookieId);
+				var cookieContainer = new CookieContainer();
+				var cookie = new Cookie(authCookie.Name, authCookie.Value, authCookie.Path, authCookie.Domain);
+				cookieContainer.Add(cookie);
+				Client.CookieContainer = cookieContainer;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		private Exception HandleResponseErrors(IRestResponse response)
+		{
+			if (response.StatusCode == HttpStatusCode.BadRequest)
+			{
+				return new VistarReponseException(JsonConvert.DeserializeObject<VistarErrorResponse>(response.Content));
+			}
+			else
+			{
+				return new Exception($"{response.StatusDescription}");
+			}
+		}
+
+		public List<VistarVenue> GetListOfVenues()
+		{
+			var request = new RestRequest(VistarApiUri.VenuesBase, Method.GET);
+			var response = Client.Execute(request);
+			if (response.StatusCode == HttpStatusCode.OK)
+			{
+				var result = JsonConvert.DeserializeObject<VistarVenueResultList>(response.Content);
+				return result.Venues;
+			}
+			else
+			{
+				throw HandleResponseErrors(response);
+			}
+		}
+
+		public VistarVenue GetVenueById(string id)
+		{
+			var request = new RestRequest(VistarApiUri.SingleVenue(id), Method.GET);
+			var response = Client.Execute(request);
+			if (response.StatusCode == HttpStatusCode.OK)
+			{
+				return JsonConvert.DeserializeObject<VistarVenue>(response.Content);
+			}
+			else
+			{
+				throw HandleResponseErrors(response);
+			}
+		}
+
+		public VistarVenue CreateVenue(VistarVenuePayload venue)
+		{
+			var request = new RestRequest(VistarApiUri.VenuesBase, Method.POST);
+			request.AddJsonBody(JsonConvert.SerializeObject(venue), "application/json");
+			var response = Client.Execute(request);
+			if (response.StatusCode == HttpStatusCode.OK)
+			{
+				//Return venue object from api call		
+				return JsonConvert.DeserializeObject<VistarVenue>(response.Content);
+			}
+			else
+			{
+				throw HandleResponseErrors(response);
+			}
+		}
+
+		public VistarVenue UpdateVenue(VistarVenueUpdatePayload venue)
+		{
+			var request = new RestRequest(VistarApiUri.SingleVenue(venue.Id), Method.PUT);
+			request.AddJsonBody(JsonConvert.SerializeObject(venue), "application/json");
+			var response = Client.Execute(request);
+			if (response.StatusCode == HttpStatusCode.OK)
+			{
+				//Return venue object from api call		
+				return JsonConvert.DeserializeObject<VistarVenue>(response.Content);
+			}
+			else
+			{
+				throw HandleResponseErrors(response);
+			}
+		}
+
+		public VistarVenue DeleteVenue(string id)
+		{
+			var request = new RestRequest(VistarApiUri.DeleteVenue(id), Method.DELETE);
+			var response = Client.Execute(request);
+			if (response.StatusCode == HttpStatusCode.OK)
+			{
+				//Return venue object from api call			
+				return JsonConvert.DeserializeObject<VistarVenue>(response.Content);
+			}
+			else
+			{
+				throw HandleResponseErrors(response);
+			}
+		}
+	}
+
+	public static class VistarExtensions
+	{
+		public static VistarVenueUpdatePayload ToVenueUpdatePayload(this VistarVenue venue)
+		{
+			var payload = new VistarVenueUpdatePayload()
+			{
+				CreativeCategoryRestrictions = venue.CreativeCategoryRestrictions,
+				Name = venue.Name,
+				VenueType = venue.VenueType,
+				ExcludedBuyTypes = venue.ExcludedBuyTypes,
+				NetworkId = venue.NetworkId,
+				Longitude = venue.Longitude,
+				AdvertiserRestrictions = venue.AdvertiserRestrictions,
+				OperatingMinutes = venue.OperatingMinutes,
+				ActivationDate = venue.ActivationDate,
+				CpmFloorCents = venue.CpmFloorCents,
+				PartnerVenueId = venue.PartnerVenueId,
+				Address = venue.Address,
+				Latitude = venue.Latitude,
+				Impressions = venue.Impressions,
+				LocationId = venue.LocationId,
+				Id = venue.Id,
+				Gvt = venue.Gvt,
+
+				//Commented out for future api updates 
+				//StaticSupported = venue.StaticSupported,
+				//VideoSupported = venue.VideoSupported,
+				//CortexSupported = venue.CortexSupported,
+				//WidthPx = venue.WidthPx,
+				//HeightPx = venue.WidthPx,
+				//StaticDurationSeconds = venue.StaticDurationSeconds,
+				//RegistrationId = venue.RegistrationId
+			};
+
+			return payload;
+		}
+
+		public static int ToVistarDayOfWeek(this DayOfWeek day)
+		{
+			return (int)day == 0 ? 6 : (int)day - 1;
+		}
+	}
+
+}
